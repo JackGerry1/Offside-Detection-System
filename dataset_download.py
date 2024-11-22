1839ac4f71adc60a5d44481676f347ab03f27ff5
jack <jackg2667@protonmail.com>
2024-11-22 18:49:08 +0000

    adding .gitignore file

--- dataset_download.py
+++ dataset_download.py
@@ -1,5 +1,15 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="oxh0PmyBG9tLQTZYozrm")
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve the API key from the environment variable
+roboflow_api_key = os.getenv("ROBOFLOW_API_KEY")
+
+# Use the API key in the Roboflow client
+rf = Roboflow(api_key=roboflow_api_key)
 project = rf.workspace("workspace1-8owu3").project("football-players-yjzti")
 version = project.version(6)
 dataset = version.download("yolov8")
